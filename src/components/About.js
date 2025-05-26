5b0c9acbd630df896a1c160cfd07b44d0154cfbe
Ury <ujamshy@yahoo.com>
2025-05-26 20:23:42 +0300

    Red => White, added warning

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#about" className="btn">
+          <a href="#" className="btn">
             read more
           </a>
         </article>
